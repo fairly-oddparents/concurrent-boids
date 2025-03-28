70560e9e1c9aa324c7ca29c991dbd0144a98cfec
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-28 13:35:59 +0100

    add multithreaded version entry point

---
+++ src/main/java/pcd/ass01/multithreaded/MultiThreadedSimulation.java
@@ -0,0 +1,25 @@
+package pcd.ass01.multithreaded;
+
+import pcd.ass01.BoidsView;
+import pcd.ass01.sequential.BoidsModel;
+import pcd.ass01.sequential.BoidsSimulator;
+
+public class MultiThreadedSimulation {
+
+	final static int N_BOIDS = 0;
+
+	final static double SEPARATION_WEIGHT = 1.0;
+	final static double ALIGNMENT_WEIGHT = 1.0;
+	final static double COHESION_WEIGHT = 1.0;
+
+	final static int ENVIRONMENT_WIDTH = 1000;
+	final static int ENVIRONMENT_HEIGHT = 1000;
+	static final double MAX_SPEED = 4.0;
+	static final double PERCEPTION_RADIUS = 50.0;
+	static final double AVOID_RADIUS = 20.0;
+
+	final static int SCREEN_WIDTH = 800;
+	final static int SCREEN_HEIGHT = 800;
+
+	//TODO: implement main
+}
