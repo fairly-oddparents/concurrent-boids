ea2ad3d6410caac93b4b683217cee7c4cc9cdf20
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2025-04-02 17:15:07 +0200

    first task-based implementation

--- src/main/java/pcd/ass01/taskBased/TaskController.java
+++ src/main/java/pcd/ass01/taskBased/TaskController.java
@@ -1,23 +1,63 @@
 package pcd.ass01.taskBased;
 
+import pcd.ass01.Boid;
 import pcd.ass01.BoidsController;
 import pcd.ass01.BoidsModel;
+import pcd.ass01.multithreaded.BarrierImpl;
+import pcd.ass01.multithreaded.BoidWorker;
+import pcd.ass01.multithreaded.api.Barrier;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class TaskController extends BoidsController {
 
     private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors() + 1;
-    private ExecutorService executor;
+    private final ExecutorService executor;
+    private List<Future> futures;
 
     public TaskController(BoidsModel model) {
         super(model);
         this.executor = Executors.newFixedThreadPool(NUM_THREADS);
+        this.futures = new ArrayList<>();
     }
 
     @Override
     public void run() {
+        super.model.setNumberBoids(super.getNumberOfBoids());
+        while (true) {
+            super.awaitRun();
+            System.out.println("Simulation...");  //TODO: remove logs
+            List<Boid> boids = this.model.getBoids();
+            for (Boid boid : boids) {
+                this.futures.add(executor.submit(new UpdateVelocityTask(boid, this.model)));
+            }
+            for (Future future : futures) {
+                try {
+                    future.get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            futures.clear();
+            for (Boid boid : boids) {
+                this.futures.add(executor.submit(new UpdatePositionTask(boid, this.model)));
+            }
+            for (Future future : futures) {
+                try {
+                    future.get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
+            var t0 = System.currentTimeMillis();
+            updateView(t0);
+
+            //TODO: executor.shutdown();
+        }
     }
 }
---
+++ src/main/java/pcd/ass01/taskBased/UpdateVelocityTask.java
@@ -0,0 +1,23 @@
+package pcd.ass01.taskBased;
+
+import pcd.ass01.Boid;
+import pcd.ass01.BoidsModel;
+
+import java.util.concurrent.Callable;
+
+public class UpdateVelocityTask implements Callable<Void> {
+
+    private final Boid boid;
+    private final BoidsModel model;
+
+    public UpdateVelocityTask(Boid boid, BoidsModel model) {
+        this.boid = boid;
+        this.model = model;
+    }
+
+    @Override
+    public Void call() throws Exception {
+        boid.updateVelocity(this.model);
+        return null;
+    }
+}
