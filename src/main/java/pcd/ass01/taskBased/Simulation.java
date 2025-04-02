38b8c961ee0cb781acd54458b2dcc3dfa7f96e69
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2025-04-02 18:17:02 +0200

    fixed taskBased\Simulation after merge

--- src/main/java/pcd/ass01/taskBased/Simulation.java
+++ src/main/java/pcd/ass01/taskBased/Simulation.java
@@ -1,25 +1,20 @@
 package pcd.ass01.taskBased;
 
+
 import pcd.ass01.BoidsController;
 import pcd.ass01.BoidsModel;
 import pcd.ass01.BoidsView;
-
-import static pcd.ass01.Constants.*;
+import pcd.ass01.multithreaded.MultithreadController;
 
 public class Simulation {
+    private static final int DEFAULT_BOIDS = 0;
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 800;
 
     public static void main(String[] args) {
-        BoidsController controller = new TaskController(new BoidsModel(
-                DEFAULT_BOIDS,
-                SEPARATION_WEIGHT, ALIGNMENT_WEIGHT, COHESION_WEIGHT,
-                ENVIRONMENT_WIDTH, ENVIRONMENT_HEIGHT,
-                MAX_SPEED,
-                PERCEPTION_RADIUS,
-                AVOID_RADIUS));
+        BoidsController controller = new TaskController(new BoidsModel(DEFAULT_BOIDS));
         BoidsView view = new BoidsView(controller, SCREEN_WIDTH, SCREEN_HEIGHT);
         controller.attachView(view);
         controller.run();
     }
-
-
 }
