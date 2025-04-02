5157ce8517bbe81f973f9b177ae9368ab5273314
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-04-02 19:12:19 +0200

    feat: implement virtual-threads-based boids simulation

---
+++ src/main/java/pcd/ass01/virtualThreads/VirtualThreadsController.java
@@ -0,0 +1,51 @@
+package pcd.ass01.virtualThreads;
+
+import pcd.ass01.Boid;
+import pcd.ass01.BoidsController;
+import pcd.ass01.BoidsModel;
+import pcd.ass01.multithreaded.MyBarrier;
+import pcd.ass01.multithreaded.api.Barrier;
+
+import java.util.List;
+
+public class VirtualThreadsController extends BoidsController {
+
+    /**
+     * Constructor for the BoidsController.
+     * @param model the model
+     */
+    public VirtualThreadsController(BoidsModel model) {
+        super(model);
+    }
+
+    @Override
+    public void run() {
+        super.model.setNumberBoids(super.getNumberOfBoids());
+        while (true) {
+            List<Boid> boids = super.model.getBoids();
+            int numBoids = boids.size();
+            Barrier velBarrier = new MyBarrier(numBoids);
+            Barrier posBarrier = new MyBarrier(numBoids + 1);
+            Barrier viewBarrier = new MyBarrier(numBoids + 1);
+            for (Boid boid : boids) {
+                Thread.ofVirtual().start(() -> {
+                    while (true) {
+                        super.awaitRun();
+                        boid.updateVelocity(super.model);
+                        velBarrier.await();
+                        boid.updatePos(super.model);
+                        posBarrier.await();
+                        viewBarrier.await();
+                    }
+                });
+            }
+            while (!super.isStopped()) {
+                var t0 = System.currentTimeMillis();
+                velBarrier.await();
+                updateView(t0);
+                viewBarrier.await();
+            }
+        }
+    }
+
+}
