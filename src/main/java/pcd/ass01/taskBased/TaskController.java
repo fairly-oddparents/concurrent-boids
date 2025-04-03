ec43fed6d142756b8b652ffe21fd2807870a4cae
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-04-03 10:47:03 +0200

    feat: implement task-based simulation for boids

---
+++ src/main/java/pcd/ass01/taskBased/TaskController.java
@@ -0,0 +1,58 @@
+package pcd.ass01.taskBased;
+
+import pcd.ass01.Boid;
+import pcd.ass01.BoidsController;
+import pcd.ass01.BoidsModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class TaskController extends BoidsController {
+
+    private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors() + 1;
+    private final ExecutorService executor;
+    private List<Future> futures;
+
+    public TaskController(BoidsModel model) {
+        super(model);
+        this.executor = Executors.newFixedThreadPool(NUM_THREADS);
+        this.futures = new ArrayList<>();
+    }
+
+    @Override
+    public void run() {
+        super.model.setNumberBoids(super.getNumberOfBoids());
+        while (true) {
+            super.awaitRun();
+            System.out.println("Simulation...");  //TODO: remove logs
+            List<Boid> boids = this.model.getBoids();
+
+            boids.forEach(boid -> this.futures.add(executor.submit(new UpdateVelocityTask(boid, this.model))));
+            waitFutures(futures);
+            futures.clear();
+
+            boids.forEach(boid -> this.futures.add(executor.submit(new UpdatePositionTask(boid, this.model))));
+            waitFutures(futures);
+            futures.clear();
+
+            var t0 = System.currentTimeMillis();
+            updateView(t0);
+            //TODO: executor.shutdown();
+        }
+    }
+
+    private void waitFutures(List<Future> futures){
+        futures.forEach(future -> {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e); //TODO:
+            }
+        });
+    }
+
+}
---
+++ src/main/java/pcd/ass01/taskBased/UpdatePositionTask.java
@@ -0,0 +1,23 @@
+package pcd.ass01.taskBased;
+
+import pcd.ass01.Boid;
+import pcd.ass01.BoidsModel;
+
+import java.util.concurrent.Callable;
+
+public class UpdatePositionTask implements Callable<Void> {
+
+    private final Boid boid;
+    private final BoidsModel model;
+
+    public UpdatePositionTask(Boid boid, BoidsModel model) {
+        this.boid = boid;
+        this.model = model;
+    }
+
+    @Override
+    public Void call(){
+        boid.updatePos(this.model);
+        return null;
+    }
+}
