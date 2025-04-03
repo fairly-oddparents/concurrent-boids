992b0a7fa9ab2e30a3306f1d205f05bbf2793a76
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2025-04-03 18:55:41 +0200

    change in screen width

--- src/main/java/pcd/ass01/taskBased/Simulation.java
+++ src/main/java/pcd/ass01/taskBased/Simulation.java
@@ -1,13 +1,13 @@
 package pcd.ass01.taskBased;
 
-
 import pcd.ass01.BoidsController;
 import pcd.ass01.BoidsModel;
 import pcd.ass01.BoidsView;
 
 public class Simulation {
+
     private static final int DEFAULT_BOIDS = 0;
-    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_WIDTH = 1000;
     public static final int SCREEN_HEIGHT = 800;
 
     public static void main(String[] args) {
