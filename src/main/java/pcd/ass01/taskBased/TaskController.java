5a0960ee6e8fd99002ae50bb29a5691fcd18b54f
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2025-04-05 18:45:54 +0200

    refactor for executor shutdown

--- src/main/java/pcd/ass01/taskBased/TaskController.java
+++ src/main/java/pcd/ass01/taskBased/TaskController.java
@@ -17,7 +17,7 @@ import java.util.concurrent.Future;
 public class TaskController extends BoidsController {
 
     private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors() + 1;
-    private final ExecutorService executor;
+    private ExecutorService executor;
     private final List<Future<Void>> futures;
 
     /**
@@ -26,7 +26,6 @@ public class TaskController extends BoidsController {
      */
     public TaskController(BoidsModel model) {
         super(model);
-        this.executor = Executors.newFixedThreadPool(NUM_THREADS);
         this.futures = new ArrayList<>();
     }
 
@@ -35,19 +34,25 @@ public class TaskController extends BoidsController {
         super.model.setNumberBoids(super.getNumberOfBoids());
         while (true) {
             super.awaitRun();
+            this.executor = Executors.newFixedThreadPool(NUM_THREADS);
             List<Boid> boids = this.model.getBoids();
+            List<UpdateVelocityTask> velocityTasks = new ArrayList<>();
+            List<UpdatePositionTask> positionTasks = new ArrayList<>();
+            boids.forEach(boid -> velocityTasks.add(new UpdateVelocityTask(boid, this.model)));
+            boids.forEach(boid -> positionTasks.add(new UpdatePositionTask(boid, this.model)));
 
-            boids.forEach(boid -> this.futures.add(executor.submit(new UpdateVelocityTask(boid, this.model))));
-            var t0 = System.currentTimeMillis();
-            waitFutures(futures);
-            futures.clear();
-
-            boids.forEach(boid -> this.futures.add(executor.submit(new UpdatePositionTask(boid, this.model))));
-            waitFutures(futures);
-            futures.clear();
-            updateView(t0);
-
-            //TODO: executor.shutdown();
+            while (!super.isStopped()) {
+                super.awaitRun();
+                velocityTasks.forEach(task -> this.futures.add(executor.submit(task)));
+                var t0 = System.currentTimeMillis();
+                waitFutures(futures);
+                futures.clear();
+                positionTasks.forEach(task -> this.futures.add(executor.submit(task)));
+                waitFutures(futures);
+                futures.clear();
+                updateView(t0);
+            }
+            executor.shutdown();
         }
     }
 
